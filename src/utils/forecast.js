67f127f248cd18b79857975146c3e0bd8df3a356
Riyaz <riyazitpro@gmail.com>
2020-01-28 16:33:06 +1100

    Added min & max temperatures in the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat, long, callback) => {
             callback(body.error, undefined)
         else {
 
-            callback(undefined, body.daily.data[0].summary + "It is currently " + body.currently.temperature + " degree out.There is a " + (body.currently.precipProbability * 100).toFixed(1) + "% chance of rain.")
+            callback(undefined, body.daily.data[0].summary + "It is currently " + body.currently.temperature + " degree out.The high today is " + body.daily.data[0].temperatureHigh + " and the low today is " + body.daily.data[0].temperatureLow + ". There is a " + (body.currently.precipProbability * 100).toFixed(1) + "% chance of rain.")
         }
     })
 }
